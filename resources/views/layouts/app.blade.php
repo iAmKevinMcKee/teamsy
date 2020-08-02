b295dddb33b63fe63eee8940e28d1cb7fb879550
Kevin McKee <mckee.kevin@gmail.com>
2020-08-02 09:53:09 -0500

    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,8 +41,8 @@
                 </div>
                 <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
                     <div class="flex-shrink-0 flex items-center px-4">
-                        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-on-dark.svg"
-                             alt="Workflow">
+                        <img class="h-8 w-auto" src="/img/logos/teamsy_on_dark.png"
+                             alt="Teamsy">
                     </div>
                     <nav class="mt-5 px-2">
                         <a href="/"
@@ -98,8 +98,8 @@
         <div class="flex flex-col w-64 bg-gray-800">
             <div class="h-0 flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
                 <div class="flex items-center flex-shrink-0 px-4">
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-on-dark.svg"
-                         alt="Workflow">
+                    <img class="h-8 w-auto" src="/img/logos/teamsy_on_dark.png"
+                         alt="Teamsy">
                 </div>
                 <!-- Sidebar component, swap this element with another sidebar if you like -->
                 <nav class="mt-5 flex-1 px-2 bg-gray-800">
