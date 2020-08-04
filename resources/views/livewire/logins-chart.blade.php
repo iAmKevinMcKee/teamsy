632b4bf6dc042e1870c3bf1ea68458cbf85387b2
Kevin McKee <mckee.kevin@gmail.com>
2020-08-03 21:06:44 -0500

    wip-charts

--- resources/views/livewire/logins-chart.blade.php
+++ resources/views/livewire/logins-chart.blade.php
@@ -19,7 +19,7 @@
             <div class="ml-4 mt-4 flex-shrink-0">
                 <span class="inline-flex rounded-md shadow-sm">
                     <button @click.prevent="chart.update({ background: true })" type="button" class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700">
-                        Refresh Logins
+                        <svg class="h-6 w-6 text-white" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>
                     </button>
                 </span>
             </div>
