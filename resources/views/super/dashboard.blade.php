ae3ae9bfa657816268d71e9855d259a60346af68
Kevin McKee <mckee.kevin@gmail.com>
2020-08-27 23:44:27 -0500

    wip

--- resources/views/super/dashboard.blade.php
+++ resources/views/super/dashboard.blade.php
@@ -116,7 +116,7 @@
                         </div>
                         <div class="ml-4 mt-4 flex-shrink-0">
                             <span class="inline-flex rounded-md shadow-sm">
-                                <button @click.prevent="chart.update({ background: true })" type="button" class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700">
+                                <button type="button" class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
                                     </svg>
@@ -126,8 +126,8 @@
                         </div>
                     </div>
                 </div>
-                <div style="height: 325px">
-                    <div id="logins-chart" style="height: 300px"></div>
+                <div class="p-10">
+                    chart goes here
                 </div>
             </div>
 
